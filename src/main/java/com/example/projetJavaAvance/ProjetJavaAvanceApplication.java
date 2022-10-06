a3b075969f491b0b542a0b3586889ac1c9ae6b77
Sunying-RONG <sunying.rong@hotmail.com>
2022-10-06 14:15:09 +0200

    init

---
+++ src/main/java/com/example/projetJavaAvance/ProjetJavaAvanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetJavaAvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJavaAvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJavaAvanceApplication.class, args);
+	}
+
+}
